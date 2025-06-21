e4ffaa4425c9f73258da15faf8ab4b5cffa0c8a1
bayustwn <khususpby@gmail.com>
2025-06-21 20:13:37 +0700

    refactor: cors

--- app.js
+++ app.js
@@ -16,7 +16,9 @@ const apikeyRoutes = require('./scr/routes/apikey');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin : "*"
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
